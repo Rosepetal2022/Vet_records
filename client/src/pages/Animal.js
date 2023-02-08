fbc9d3f60938b64b97ee58a3862a89a9f0c9e138
LaLacou <108777796+llacourciere@users.noreply.github.com>
2023-02-07 20:26:52 -0800

    updates typedefs/queries and fixed length reading error

--- client/src/pages/Animal.js
+++ client/src/pages/Animal.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_ANIMALS, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import AddAnimalModal from '../components/Modal/AddAnimaModal';
 import AnimalList from '../components/AnimalList';
+import { Spinner } from 'reactstrap';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const Animal = () => {
   const { username: userParam } = useParams();
-  const data = useQuery(QUERY_USER, { variables: { username: userParam }})
-  const user = data?.user || [];
+  const { loading, data } = useQuery(QUERY_ANIMALS);
+  const {data: userData } = useQuery(QUERY_ME);
+  const loggedIn = Auth.loggedIn();
+
+  const animals = data?.animal || [];
+  //const me = data?.me || [];
+  
+  //console.log(me);
+
+
 
   return (
-    <main>
-        <div >
-        <h3>{user.username}'s Pets</h3>
-        
-            <AnimalList
-                animal={user.animal}
-                title=""
-                    />
-                    
-        </div>
-    
-    </main>
+      <main>
+          <div className=''>
+              {loggedIn && (
+                  <AddAnimalModal />
+              )}
+          </div>
+          <div className={`${loggedIn && ''}`}>
+          {loading ? (
+                    <Spinner color='primary'>Loading...</Spinner>
+                ) :
+               (
+                  <AnimalList
+                      animals={animals}
+                      title="My Pets"
+                  />
+              )}
+          </div>
+      </main>
   );
 };
 
